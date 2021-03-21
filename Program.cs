812f6c304eeb93812d87429de0b5755fc2243cbe
camila <camilasangobar@gmail.com>
2021-03-21 19:13:28 -0300

    medidas

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Medidas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+    
+            
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("-----Equivalência------");
+            Console.ResetColor();
+            Console.WriteLine("digite um número:");
+            double m = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine();
+            double cm = m * 100;
+            double km = m / 1000;
+            Console.WriteLine($"{cm} cm");
+            Console.WriteLine($"{m} m");
+            Console.WriteLine($"{km}km");
+
+        }
+    }
+}
